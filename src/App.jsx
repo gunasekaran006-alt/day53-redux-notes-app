8f16561be42beb70369deba60c50f8859ae9b240
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-04-28 22:18:22 +0530

    updates-dependencies

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NotesForm from './components/NotesForm'
+import NotesList from './components/NotesList'
+
+function App() {
+  return (
+    <div>
+      <NotesForm />
+      <NotesList />
+    </div>
+  )
+}
+
+export default App
